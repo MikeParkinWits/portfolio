5c567fcbcef3c7f50c5c47fe2db7a6bb96b54133
Michael Parkin <1894979@students.wits.ac.za>
2023-01-11 18:07:33 +0200

    Setting up React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+
+      <main>
+        <Routes>
+          <Route>
+            <Route path="/portfolio" element={<Home />} />
+          </Route>
+        </Routes>
+      </main>
     </div>
   );
 }
