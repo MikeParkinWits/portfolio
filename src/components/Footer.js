e8959c2579f1f3df4ee622dfdb40163ee692558c
Michael Parkin <1894979@students.wits.ac.za>
2023-01-12 14:56:40 +0200

    Added LinkedIn to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "../styles/footer.css";
 
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { NavLink } from "react-router-dom";
+
 export default class Footer extends Component {
   render() {
     return (
@@ -10,6 +13,14 @@ export default class Footer extends Component {
             <p className="p-name">Michael Parkin</p>
             <p>|</p>
             <time dateTime="2023">2023</time>
+          </section>
+          <section className="secondary-footer-contents">
+            <a
+              href="https://www.linkedin.com/in/michael-parkin-a43349169/"
+              target={"_blank"}
+            >
+              <LinkedInIcon />
+            </a>
           </section>
         </footer>
       </>
