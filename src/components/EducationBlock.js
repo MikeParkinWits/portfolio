ec77976bd725ba5c13799f9f7a8230bffd2be844
Michael Parkin <mparkin6@gatech.edu>
2023-10-05 23:02:25 -0400

    Made EducationBlock more Reusable

--- src/components/EducationBlock.js
+++ src/components/EducationBlock.js
@@ -3,21 +3,29 @@ import React, { Component } from "react";
 import WitsLogo from "../assets/WitsLogo.png";
 
 export default class EducationBlock extends Component {
+  static defaultProps = {
+    educationName: "University Name",
+    educationImage: WitsLogo,
+    educationQualification: "Education Qualification",
+    educationYear: "2019 - 2022",
+  };
+
   render() {
+    const {
+      educationImage,
+      educationName,
+      educationQualification,
+      educationYear,
+    } = this.props;
     return (
       <section className="education-block">
-        <img src={WitsLogo} width="1000" height="1000" />
+        <img src={educationImage} width="1000" height="1000" />
         <section className="education-block-info">
           <header>
-            <h3 className="education-block-title">
-              The University of the Witwatersrand
-            </h3>
+            <h3 className="education-block-title">{educationName}</h3>
           </header>
-          <p>
-            Bachelor of Arts in Digital Arts (Game, and Interactive Media,
-            Design)
-          </p>
-          <time>2019 - 2022</time>
+          <p>{educationQualification}</p>
+          <time>{educationYear}</time>
         </section>
       </section>
     );
